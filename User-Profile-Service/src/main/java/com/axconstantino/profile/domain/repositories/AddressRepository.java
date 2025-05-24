a1a21bb2475e58590b09c3fe4bdc590f9d2b8666
Axel Constantino Olvera <constantinoolveraa@gmail.com>
2025-05-23 18:23:39 -0600

    Add findAllByUserProfileId(UUID userProfileId) method

--- User-Profile-Service/src/main/java/com/axconstantino/profile/domain/repositories/AddressRepository.java
+++ User-Profile-Service/src/main/java/com/axconstantino/profile/domain/repositories/AddressRepository.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public interface AddressRepository {
     void save(Address address);
     Address findById(UUID id);
-    List<Address> findByUserProfileId(UUID userProfileId);
+    List<Address> findAllByUserProfileId(UUID userProfileId);
     Address findByIdAndUserProfileId(UUID id, UUID userProfileId);
     void deleteByIdAndUserProfileId(UUID id, UUID userProfileId);
     void update(UUID existingAddress, Address newAddress);
